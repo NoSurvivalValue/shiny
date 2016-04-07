0387bb7e7b6095a72051c54157062b5d2b910b07
NoSurvivalValue <zoot.wowsquirrel@gmail.com>
2016-04-07 18:53:53 +0300

    Create ui.R

---
+++ my_app/ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+library(datasets)
+library(ggplot2)
+
+words <- read.csv("C:/Users/M/Downloads/all_words.csv",
+                 header = T)
+
+shinyUI(
+  fluidPage(
+    titlePanel("One thousand Most Frequently Used Words"),
+    headerPanel("Это приложение показывает 1000 самых частотных слов некоторых языков. Данные основаны на материалах сайта http://www.101languages.net/common-words/. Нельзя гарантировать, что данные собраны хорошо (процесс сбора данных на сайте не описан), ровно как и не стоит верить переводам."),
+    fluidRow(
+      column(4,
+             selectInput("language",
+                         "Language:",
+                         c("All",
+                           unique(as.character(words$language))))
+      )
+    ),
+    fluidRow(
+      DT::dataTableOutput("table")
+    )
+  )
+)
